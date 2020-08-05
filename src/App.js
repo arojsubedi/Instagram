6025d55dd5a94d7066d8a9bd0f4875825705a4a4
Aroj Subedi <arojsubedi@gmail.com>
2020-08-06 02:34:26 +0530

    Posts loading using props
    Signed-off-by: Aroj Subedi <arojsubedi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 import NavBar from './NavBar.js';
 import Posts from './Posts.js';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      userName:'Aroj Subedi',
+      imageCaption : 'Such a beautiful picture. Speaks volume. #wonderful',
+      imageUrl:'https://images.unsplash.com/photo-1542044896530-05d85be9b11a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      userName:'Steven Gerrard',
+      imageCaption:'Thats dope mate',
+      imageUrl:'https://images.unsplash.com/photo-1594433743374-1b4b98798c0b?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      userName:'Roberto Firmino',
+      imageCaption :'No words to describe',
+      imageUrl:'https://images.unsplash.com/photo-1593088177922-aa1c5055c7b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      userName:'Sadio Mane',
+      imageCaption :'Creative shot',
+      imageUrl:'https://images.unsplash.com/photo-1593983649075-9fd447bb280a?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    }
+  ]);
   return (
     <React.Fragment>
         <NavBar />
+        <Posts  userName='Aroj' imageCaption='Hi there' imageUrl='https://images.unsplash.com/photo-1542044896530-05d85be9b11a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' />
+        {
+          posts.map((nPosts)=>{
+            // <Posts />
+            <Posts  userName={nPosts.userName} imageCaption={nPosts.imageCaption} imageUrl={nPosts.imageUrl} />
+          })
+        }
+       
+        {/* <Posts />
         <Posts />
-        <Posts />
-        <Posts />
-        <Posts />
+        <Posts /> */}
     </React.Fragment>
   );
 }
--- src/Posts.js
+++ src/Posts.js
@@ -2,30 +2,26 @@ import React from 'react';
 import './Posts.css';
 import Avatar from '@material-ui/core/Avatar';
 
-class Posts extends React.Component{
-    render(){
+function Posts(props){
         return(
-            <React.Fragment>
                 <div className="posts__body">
                     <div className="posts__section">
                         <div className="section__avatar">
-                            <Avatar alt="Aroj Subedi" src="/static/images/avatar/1.jpg" />
-                            <div className="avatar__text"><h4>Aroj Subedi</h4></div>
+                            <Avatar alt={props.userName} src="/static/images/avatar/1.jpg" />
+                            <div className="avatar__text"><h4>{props.userName}</h4></div>
                         </div>
                         <div className="section__image">
                             <img 
-                                src='https://images.unsplash.com/photo-1542044896530-05d85be9b11a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                                src={props.imageUrl}
                                 className="image__style"
                             />
                         </div>
                         <div className="section__captions">
-                            <h5><strong>arojsubedi</strong> Such a beautiful picture. Speaks volume. #wonderful</h5>
+                            <h5>{props.imageCaption}</h5>
                         </div>
                     </div>
                 </div>
-            </React.Fragment>
         )
-    }
 }
 
 export default Posts;
